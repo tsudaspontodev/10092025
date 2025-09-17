557368242598802d99b37ac825d9b8363314ffd6
Marcos B. Tsuda <tsudaspontodev@gmail.com>
2025-09-16 22:39:21 -0300

    adicionado os arquivos css e js
    Finalizado a estilização da página 
    Realizado a programação do js

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict'; //Ajuda evitar erros no JS
+
+const inputNome = document.getElementById('nome');
+// campo de texto do nome
+
+const inputAno = document.getElementById('ano');
+// campo de texto do ano
+
+const botaoCalcular = document.getElementById('btn-calcular');
+// botão de calcular a idade
+
+const resultado = document.getElementById('resultado');
+// parágrafo onde aparece o resultado
+
+const anoAtual = new Date().getFullYear();
+//descobrir o ano atual
+
+botaoCalcular.addEventListener('click', () => {
+    const nome = inputNome.value.trim();
+    const anoNascStr = inputAno.value.trim();
+    const anoNasc = Number(anoNascStr);
+    const anoValido = Number.isInteger(anoNasc) && anoNasc >= 1900 && anoNasc <= anoAtual;
+
+    if (!anoValido) {
+        resultado.textContent = 'Por favor, coloque um ano válido 1900 - 2100';
+        return; //Saí da função sem continuar
+    }
+
+    const idade = anoAtual - anoNasc
+    resultado.textContent = (`${nome}, tem ${idade} anos de idade`);
+});
+
